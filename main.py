8d54c6bf0357b506e533d6e600a9b3673a0ff2f7
Ilia <istudyatuni@gmail.com>
2022-05-01 20:36:33 +0300

    improvements

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from os import mkdir, system
+from typing import Iterable
 import os.path
 import requests as req
 
@@ -14,7 +15,7 @@ FILTERED_FILE = 'filtered.txt'
 
 get_data = lambda: req.get(DATA_URL).json()
 
-def write_list(filename: str, data: list, *, acl = False):
+def write_list(filename: str, data: Iterable[str], *, acl = False):
 	header = ACL_HEADER if acl else ''
 	with open(filename, 'w') as file:
 		file.write(header + '\n'.join(data) + '\n')
@@ -26,6 +27,14 @@ def read_list(filename: str):
 
 # https://stackoverflow.com/a/71922468
 to_puny = lambda s: s.encode('idna').decode()
+def from_puny(s):
+	try:
+		return s.encode().decode('idna')
+	except UnicodeDecodeError:
+		return s
+	except UnicodeError as e:
+		print(f'[decode] {e}:', s)
+		return s
 
 def convert_domains(domains: list[str]) -> list[str]:
 	uniq_domains = set(to_puny(d) for d in domains)
@@ -42,14 +51,16 @@ def convert_domains(domains: list[str]) -> list[str]:
 def main():
 	print('[main] fetch data')
 	domains = get_data()
-	write_list(ORIGINAL_FILE, domains)
+	write_list(ORIGINAL_FILE, (from_puny(d) for d in domains))
 
 	system('./filter.sh')
 
 	domains = read_list(DOMAINS_FILE)
 	filtered_domains = read_list(FILTERED_FILE)
 
-	mkdir(OUT_DIR)
+	if not os.path.exists(OUT_DIR):
+		mkdir(OUT_DIR)
+
 	print('[main] convert main domains')
 	write_list(os.path.join(OUT_DIR, 'ru.acl'), convert_domains(domains), acl=True)
 	print('[main] convert filtered domains')
